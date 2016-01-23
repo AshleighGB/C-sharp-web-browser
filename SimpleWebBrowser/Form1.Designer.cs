fa8ab3492103cbd778fa990e0daf27e7810f6e8e
Joshua Wareing <joshuawareing@outlook.com>
2016-01-23 22:01:12 +0000

    .

---
+++ SimpleWebBrowser/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace SimpleWebBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kimJongMemeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.catsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dogsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.chineseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.randomShitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.goHomeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.goHomeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1062, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kimJongMemeToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // kimJongMemeToolStripMenuItem
+            // 
+            this.kimJongMemeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.catsToolStripMenuItem,
+            this.dogsToolStripMenuItem,
+            this.chineseToolStripMenuItem,
+            this.randomShitToolStripMenuItem});
+            this.kimJongMemeToolStripMenuItem.Name = "kimJongMemeToolStripMenuItem";
+            this.kimJongMemeToolStripMenuItem.Size = new System.Drawing.Size(113, 22);
+            this.kimJongMemeToolStripMenuItem.Text = "Memes";
+            this.kimJongMemeToolStripMenuItem.ToolTipText = "Meme";
+            this.kimJongMemeToolStripMenuItem.Click += new System.EventHandler(this.kimJongMemeToolStripMenuItem_Click);
+            // 
+            // catsToolStripMenuItem
+            // 
+            this.catsToolStripMenuItem.Name = "catsToolStripMenuItem";
+            this.catsToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.catsToolStripMenuItem.Text = "Cats";
+            this.catsToolStripMenuItem.Click += new System.EventHandler(this.catsToolStripMenuItem_Click);
+            // 
+            // dogsToolStripMenuItem
+            // 
+            this.dogsToolStripMenuItem.Name = "dogsToolStripMenuItem";
+            this.dogsToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.dogsToolStripMenuItem.Text = "Dogs";
+            this.dogsToolStripMenuItem.Click += new System.EventHandler(this.dogsToolStripMenuItem_Click);
+            // 
+            // chineseToolStripMenuItem
+            // 
+            this.chineseToolStripMenuItem.Name = "chineseToolStripMenuItem";
+            this.chineseToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.chineseToolStripMenuItem.Text = "Chinese";
+            this.chineseToolStripMenuItem.Click += new System.EventHandler(this.chineseToolStripMenuItem_Click);
+            // 
+            // randomShitToolStripMenuItem
+            // 
+            this.randomShitToolStripMenuItem.Name = "randomShitToolStripMenuItem";
+            this.randomShitToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.randomShitToolStripMenuItem.Text = "Random Shit";
+            this.randomShitToolStripMenuItem.Click += new System.EventHandler(this.randomShitToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(113, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // goHomeToolStripMenuItem
+            // 
+            this.goHomeToolStripMenuItem.Name = "goHomeToolStripMenuItem";
+            this.goHomeToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.goHomeToolStripMenuItem.Text = "Go Home";
+            this.goHomeToolStripMenuItem.ToolTipText = "Takes you to the homepage.";
+            this.goHomeToolStripMenuItem.Click += new System.EventHandler(this.goHomeToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(123, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(927, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "http://google.com";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(996, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(54, 20);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(2, 41);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1060, 353);
+            this.webBrowser1.TabIndex = 3;
+            this.webBrowser1.Url = new System.Uri("http://google.com", System.UriKind.Absolute);
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            this.webBrowser1.ProgressChanged += new System.Windows.Forms.WebBrowserProgressChangedEventHandler(this.webBrowser1_ProgressChanged);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar1,
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 397);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1062, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "Current Status";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(82, 17);
+            this.toolStripStatusLabel1.Text = "Current Status";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1062, 419);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Web Browser";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripMenuItem goHomeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kimJongMemeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem catsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dogsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem chineseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem randomShitToolStripMenuItem;
+    }
+}
+
---
+++ SimpleWebBrowser/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleWebBrowser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();   
+        }
+        // close the program.
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        // when Go! button is clicked.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            NavigateToPage();
+        }
+        // set up actions to be carried out when navigating to a webpage.
+        private void NavigateToPage()
+        {
+            button1.Enabled = false;
+            textBox1.Enabled = false;
+            webBrowser1.Navigate(textBox1.Text);
+            toolStripStatusLabel1.Text = "Loading...";
+
+        }
+        // wait for a specific key to be pressed which should be enter.
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)ConsoleKey.Enter) {
+                NavigateToPage();
+                e.Handled = true;
+            }
+        }
+        // when the browser has finished loading the webpage, run this function.
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            button1.Enabled = true;
+            textBox1.Enabled = true;
+            toolStripStatusLabel1.Text = "Done.";
+            textBox1.Text = webBrowser1.Url.ToString();
+        }
+
+        private void webBrowser1_ProgressChanged(object sender, WebBrowserProgressChangedEventArgs e)
+        {
+            // make sure that the progress bar doesn't do anything unless current progress AND maximum progress is greater than 0
+            if (e.CurrentProgress > 0 && e.MaximumProgress > 0)
+            {
+
+                toolStripProgressBar1.ProgressBar.Value = (int)(e.CurrentProgress * 100 / e.MaximumProgress);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // do nothing because nothing.
+        }
+        private void goHomeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // my workaround for the homepage button.
+            // set url of homepage
+            textBox1.Text = "https://encrypted.google.com";
+            // go to homepage.
+            NavigateToPage();
+        }
+        private void catsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (HtmlElement image in webBrowser1.Document.Images) {
+                image.SetAttribute("src", "https://i.ytimg.com/vi/icqDxNab3Do/maxresdefault.jpg");
+            }
+        }
+
+        private void kimJongMemeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //do nothing because nothing.
+        }
+
+        private void dogsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (HtmlElement image in webBrowser1.Document.Images)
+            {
+                image.SetAttribute("src", "http://designbump.com/wp-content/uploads/2014/07/funny-dog-91_1.jpg");
+            }
+        }
+
+        private void chineseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (HtmlElement image in webBrowser1.Document.Images)
+            {
+                image.SetAttribute("src", "http://images.complex.com/complex/image/upload/t_article_image/fltnuazwwpoh8eat1j3k.jpg");
+            }
+        }
+
+        private void randomShitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (HtmlElement image in webBrowser1.Document.Images)
+            {
+                image.SetAttribute("src", "http://img.memecdn.com/rmx-oh-shit_o_813170.jpg");
+            } 
+        }
+    }
+}
